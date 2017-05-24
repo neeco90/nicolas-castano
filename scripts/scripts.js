9e8cbb238feb22ec49ecd91f28aff1d084601ae8
ncastano-ext-technisys <ncastano-ext@technisys.com>
2017-05-24 18:01:29 -0300

    todo lo que faltaba

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,6 +24,35 @@ function circleCharts(){
     js.animate(0.78);
 }
 
+function lineCharts(){
+    //line chart
+    barParams = {
+        strokeWidth: 2,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#00ffa8',
+        trailColor: '#eee',
+        trailWidth: .1,
+        svgStyle: {width: '100%', height: '100%'}                         
+    };
+
+    var illustrator = new ProgressBar.Line('#illustrator', barParams);
+    var photoshop = new ProgressBar.Line('#photoshop', barParams);
+    var uidesign = new ProgressBar.Line('#uidesign', barParams);
+    var branding = new ProgressBar.Line('#branding', barParams);
+
+    illustrator.animate(0.9);  // Number from 0.0 to 1.0
+    photoshop.animate(0.8);
+    uidesign.animate(0.85);
+    branding.animate(0.85);
+}
+
+function openMenu(){
+    if(!$('.main-header_nav_menu-list').hasClass('opened')){
+        $('.main-header_nav_menu-list').toggleClass('opened');
+    }
+}
+
 ///////// INICIALIZACIÓN DE FUNCIONES /////////
 
 $(document).ready(function(){
@@ -31,6 +60,7 @@ $(document).ready(function(){
 
     $('#personalData').waypoint(function(direction) {
         circleCharts();
+        lineCharts();
         this.destroy();
     }, {
         offset: '500'
