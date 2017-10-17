cc0cbbde19b47e0ef3980839c835e1a066cf7081
ncastano-ext-technisys <ncastano-ext@technisys.com>
2017-10-17 18:16:56 -0300

    correcciones

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -33,23 +33,41 @@ function lineCharts(){
         color: '#00ffa8',
         trailColor: '#eee',
         trailWidth: .1,
-        svgStyle: {width: '100%', height: '100%'}                         
+        svgStyle: {width: '100%', height: '100%'},
+        text: {
+            style: {
+              color: '#fff',
+              padding: 0,
+              margin: 0,
+              transform: null
+            },
+            autoStyleContainer: false
+        },
+        step: (state, bar) => {
+            bar.setText(Math.round(bar.value() * 100) + ' %');
+        }                         
     };
 
     var illustrator = new ProgressBar.Line('#illustrator', barParams);
     var photoshop = new ProgressBar.Line('#photoshop', barParams);
+    var xd = new ProgressBar.Line('#xd', barParams);
     var uidesign = new ProgressBar.Line('#uidesign', barParams);
+    var accesibility = new ProgressBar.Line('#accesibility', barParams);
     var branding = new ProgressBar.Line('#branding', barParams);
 
     illustrator.animate(0.9);  // Number from 0.0 to 1.0
     photoshop.animate(0.8);
+    xd.animate(0.75);
     uidesign.animate(0.85);
+    accesibility.animate(0.7);
     branding.animate(0.85);
 }
 
 function openMenu(){
     if(!$('.main-header_nav_menu-list').hasClass('opened')){
         $('body').css('overflow-y','hidden');
+        $('.main-header_nav_menu').hide();
+        $('.menu-icon_close').show();
         $('.main-header_nav_menu-list').fadeIn('fast', function(){
             $('.main-header_nav_menu-list').addClass('opened');
         });
@@ -59,6 +77,8 @@ function openMenu(){
 function closeMenu(){
     if($('.main-header_nav_menu-list').hasClass('opened')){
         $('body').css('overflow-y','auto');
+        $('.main-header_nav_menu').show();
+        $('.menu-icon_close').hide();
         $('.main-header_nav_menu-list').fadeOut('fast', function(){
             $('.main-header_nav_menu-list').removeClass('opened');
         });
@@ -76,5 +96,52 @@ $(document).ready(function(){
         this.destroy();
     }, {
         offset: '500'
+    });
+
+    
+    // SMOOTH SCROLL!
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+    ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+            } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+            };
+        });
+        }
+    }
+    });
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= 300) {
+            $(".main-header_nav").addClass("fixed");
+        } else {
+            $(".main-header_nav").removeClass("fixed");
+        }
     });
 })
